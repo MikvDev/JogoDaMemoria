f34bea4844e34808a788efa6d43d95a6d581999a
Miguel Vargas Ferreira <miguelferreira8967@gmail.com>
2026-04-29 16:32:18 -0300

    BugFix: Corrige estrutura das tags HTML

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,18 +1,15 @@
-const input = document.querySelector('.login_input')
-const button = document.querySelector('.login__button')
-
-
-
-
+const input = document.querySelector('.loginInput');
+const button = document.querySelector('.loginButton');
 
 const validateInput = ({ target }) => {
-    if (target.value.length > 2) {;
-        button.removeAttribute('disabled')
-        return
-    } 
-        button.setAttribute('disabled', '')
+   
+    if (target.value.trim().length > 2) {
+        
+        button.removeAttribute('disabled');
+    } else {
+        
+        button.setAttribute('disabled', '');
     }
-
-
+}
 
 input.addEventListener('input', validateInput);
